65f9db62e61fc84e67a26870805e6a7a5da9f3b3
Pimnipa Srisaengsup <tbpsrisa@liverpool.ac.uk>
2026-04-20 12:51:02 +0100

    Add files via upload

---
+++ Report/Results/Part_3/part_3_code.sh
@@ -0,0 +1,23 @@
+# Find Target Genes (b3593, b0356, b2952) in GFF
+
+echo "=== Find Target Genes in GFF ==="
+grep -E "b3593|b2952|b0356" "$RESULT/bakta_GN6"/*.gff
+
+# Extract Target Gene Protein Sequences (FAA)
+
+echo "=== Extract Protein Sequences ==="
+grep -A 1 "b3593" "$RESULT/bakta_GN6"/*.faa > b3593_protein.fasta
+grep -A 1 "b0356" "$RESULT/bakta_GN6"/*.faa > b0356_protein.fasta
+grep -A 1 "b2952" "$RESULT/bakta_GN6"/*.faa > b2952_protein.fasta
+
+# Extract Target Gene Nucleotide Sequences (FFN)
+
+echo "=== Extract Nucleotide Sequences ==="
+grep -A 1 "b3593" "$RESULT/bakta_GN6"/*.ffn > b3593_nucleotide.fasta
+grep -A 1 "b0356" "$RESULT/bakta_GN6"/*.ffn > b0356_nucleotide.fasta
+grep -A 1 "b2952" "$RESULT/bakta_GN6"/*.ffn > b2952_nucleotide.fasta
+
+# Extract by gene name (gltB, frmA, lacZ) from SPAdes annotation
+grep -A 1 "gltB" "$RESULT/prokka_GN6/GN6_prokka.ffn"  > target_genes_nucleotide.fasta
+grep -A 1 "frmA" "$RESULT/prokka_GN6/GN6_prokka.ffn"  > target_genes_nucleotide.fasta
+grep -A 1 "lacZ" "$RESULT/prokka_GN6/GN6_prokka.ffn"  > target_genes_nucleotide.fasta
